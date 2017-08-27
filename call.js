cbc2c689f75dfb1fd3d8c5eaa8a49778199af523
Felix <felix@mobilpay.ro>
2017-08-27 21:17:58 +0300

    added a call contract example changed the compilation to generate a file with the deployed contract address

---
+++ call.js
@@ -0,0 +1,27 @@
+const Web3 = require('../ethereum/node_modules/web3/index.js')
+const web3 = new Web3()
+const config = require('../ethereum/config')
+web3.setProvider(new web3.providers.HttpProvider(config.provider.dev))
+const address = "0xB98B9dCfDF06095408530C395Dea296103469257"
+const fs = require('fs')
+var abi = fs.readFileSync("bin/BucJSToken.abi")
+var contract = web3.eth.contract(JSON.parse(abi))
+
+var instance = require('./contract.json')
+if(instance != null && instance.contract != null) {
+    var tokenContract = contract.at(instance.contract)
+    /*
+    since the contract was deployed using the first account in personal, that account owns all the tokens
+    */ 
+    var sender = web3.personal.listAccounts[0]
+    console.log("sending from", sender)
+    web3.personal.unlockAccount(sender,"")
+
+    /*
+    Once we unlocked the account we simply need to call the transfer function of the contract
+    Things are relatively easy here as we let the RPC endpoint (a node also holding the accounts) do all the transaction signing
+    If we would need to send it from an account that is not on the node (thus not accesible/unlock-able via web3.personal), things are slightly more difficult
+    */ 
+    var gasLimit = 100000
+    console.log("transaction sent with hash",tokenContract.transfer(address,2000,{from:sender,gas:gasLimit}))
+}
--- compile-deploy.js
+++ compile-deploy.js
@@ -2,23 +2,20 @@ const Web3 = require('../ethereum/node_modules/web3/index.js')
 const web3 = new Web3()
 const config = require('../ethereum/config')
 web3.setProvider(new web3.providers.HttpProvider(config.provider.dev))
-const contractFile = "contract.sol";
-const fs = require('fs');
-const exec = require('child_process').execSync;
+const contractFile = "contract.sol"
+const fs = require('fs')
+const exec = require('child_process').execSync
 console.log("compiling",contractFile)
-exec(`solc --bin --abi --optimize --overwrite -o bin contract.sol`);
+exec(`solc --bin --abi --optimize --overwrite -o bin contract.sol`)
 /*pay attention to file names here!*/
-var abi = fs.readFileSync("bin/BucJSToken.abi");
-var compiled = "0x" + fs.readFileSync("bin/BucJSToken.bin");
+var abi = fs.readFileSync("bin/BucJSToken.abi")
+var compiled = "0x" + fs.readFileSync("bin/BucJSToken.bin")
 console.log("we should now have in 'bin' the abi and the compiled contract")
 var contract = web3.eth.contract(JSON.parse(abi))
+
 /*
-The contract constructor has 4 defined parameters
-uint256 initialSupply,
-string tokenName,
-uint8 decimalUnits,
-string tokenSymbol
-so we need to supply 4 params to the contract.new call below
+This is an account on the node that accepts RPC requests. web3.personal is usually _not_ enabled
+Also, it's a bad practice to leave 'personal' accesible via RPC, not to mention that there's no password on the account
 */
 var deployer = web3.personal.listAccounts[0]
 console.log("deploying from", deployer)
@@ -33,19 +30,28 @@ console.log("it's about",gasLimit,"gas")
 var tx = {
     gas: gasLimit+10,
     from: deployer,
-    data:compiled
+    data: compiled
 }
+/*
+The contract constructor has 4 defined parameters
+uint256 initialSupply,
+string tokenName,
+uint8 decimalUnits,
+string tokenSymbol
+so we need to supply 4 params to the contract.new call below
 
-/*callback triggers twice, once on send, once when it is mined. 
+Note that callback triggers twice, once on send, once when the contract deployed is mined. 
 When sent there's no address allocated yet
 */
 contract.new(1000000,"BucharestJS TestToken",0,"BJSTT", tx ,(e, c) => {
-    if(e){
-            console.log("Error!",e);return;
+    if(e) {
+        console.log("Error!",e)
+        return
     }       
     if(c.address == null) {
         console.log("Contract sent in transaction", c.transactionHash)
     } else {
         console.log("Contract mined with address" ,c.address , ", transactionHash was" , c.transactionHash)
+        fs.writeFileSync("contract.json",JSON.stringify({contract:c.address,transaction:c.transactionHash}))
     }    
 })
